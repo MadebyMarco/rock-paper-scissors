b3147217ddff0788d272db9f71c4c8dbf71d1bef
MadebyMarco <marco3xmedina@gmail.com>
2022-12-27 11:17:33 -0800

    Add computer's choice

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function getComputerChoice (
-    //randomly return either rock paper or scissors for the computer to use against you
-)
 
+
+let computerOptions = ["rock", "paper", "scissors"];
+
+function getComputerChoice () {
+     //randomly return either rock paper or scissors for the computer to use against you)
+     let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+     return computerChoice;
+}
+
+
+/**
 function singleroundofRPS (takes two parameters, playerSelection, computerSelection) {
     // output will return a string that declares the winner of the round like "You lose! Paper beats Rock"
     // playerSelection will be case-insensitive 
@@ -14,3 +22,4 @@ function game(let i = 0; i < 5; i++) {
 }
 
 //use prompt to get input from the user
+*/
