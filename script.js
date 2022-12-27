4d350e600f764590a90bd4292852ad8a84187909
MadebyMarco <marco3xmedina@gmail.com>
2022-12-27 10:47:40 -0800

    Add pseudo code

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice (
+    //randomly return either rock paper or scissors for the computer to use against you
+)
+
+function singleroundofRPS (takes two parameters, playerSelection, computerSelection) {
+    // output will return a string that declares the winner of the round like "You lose! Paper beats Rock"
+    // playerSelection will be case-insensitive 
+    //reurn the results of the function
+    //console.log() the result of each round and the winner at the end
+}
+
+function game(let i = 0; i < 5; i++) {
+    //loop the single round of rock paper scissors 
+}
+
+//use prompt to get input from the user
