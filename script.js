f5620cd474db055ad66f234198bde03ed9f42732
MadebyMarco <marco3xmedina@gmail.com>
2022-12-27 13:42:53 -0800

    Update for laptop

--- script.js
+++ script.js
@@ -2,21 +2,31 @@
 
 let computerOptions = ["rock", "paper", "scissors"];
 
-function getComputerChoice () {
+function getComputerChoice() {
      //randomly return either rock paper or scissors for the computer to use against you)
-     let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
-     return computerChoice;
+     let computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+     return computerSelection;
 }
 
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
 
-/**
-function singleroundofRPS (takes two parameters, playerSelection, computerSelection) {
+let playerSelection = prompt("Enter rock, paper, or scissors", "");
+//make case insensitive 
+
+playRound();
+
+function playRound(playerSelection, computerSelection) {
+    
     // output will return a string that declares the winner of the round like "You lose! Paper beats Rock"
     // playerSelection will be case-insensitive 
     //reurn the results of the function
     //console.log() the result of each round and the winner at the end
 }
 
+
+
+/**
 function game(let i = 0; i < 5; i++) {
     //loop the single round of rock paper scissors 
 }
