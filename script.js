21373b73dbf493804cf0046030100cec1ec38003
MadebyMarco <marco3xmedina@gmail.com>
2022-12-27 15:21:35 -0800

    Create function that plays a round and returns the result as a string

--- script.js
+++ script.js
@@ -7,22 +7,46 @@ function getComputerChoice() {
      let computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)]
      return computerSelection;
 }
-
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-let playerSelection = prompt("Enter rock, paper, or scissors", "");
-//make case insensitive 
-
-playRound();
+let result;
 
 function playRound(playerSelection, computerSelection) {
     
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "You win! Rock beats Scissors";
+        console.log;("You win.");
+    }else if (playerSelection === "scissors" && computerSelection === "rock") {
+        result = "You lose! Rock beats Scissors";
+        console.log("You lose.");
+    }else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "You win! Paper beats rock";
+        console.log("You win.");
+    }else if (playerSelection === "rock" && computerSelection === "paper") {
+        result = "You lose! Paper beats rock "; 
+        console.log("You lose.");
+    }else if (playerSelection === "scissors" && computerSelection === "paper") {
+        result = "You win! Scissors beats paper";
+        console.log("You win.");
+    }else if (playerSelection === "paper" && computerSelection === "scissors") {
+        result = "You lose! Scissors beats paper"; 
+        console.log("You lose.");
+    }else if (playerSelection === computerSelection) {
+        result = "Its a draw!";
+        console.log("Tie");
+    }else result = null, console.log("Enter a valid option.") ;
+
+    return result;
     // output will return a string that declares the winner of the round like "You lose! Paper beats Rock"
     // playerSelection will be case-insensitive 
     //reurn the results of the function
     //console.log() the result of each round and the winner at the end
 }
+
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+let userInput = prompt("Enter rock, paper, or scissors", "");
+let playerSelection = userInput.toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection));
 
 
 
